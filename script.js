8bdb39fb5b9bb52071d31236edcd8a271eada087
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-11-24 14:19:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+		function printAll()
+		{
+
+			for(let i=1;i<=20;i++)
+				{
+						if(i%3==0){console.log('Fizz')}
+					    else if(i%5==0){console.log('Buzz')}
+						    else if(i%15==0){console.log('FIzzBuzz')}
+					else console.log(i);
+				}
+		}
+
+printAll();
